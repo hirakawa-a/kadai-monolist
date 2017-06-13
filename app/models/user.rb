4554d76b651fc8b70a5b0efd91543b40a7a900db
atsushi hirakawa <atsushi-hirakawa@dac.co.jp>
2017-06-13 09:20:45 +0000

    Have

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
 	has_many :items, through: :ownerships
 	has_many :wants
 	has_many :want_items, through: :wants, class_name: 'Item', source: :item
-end
+	has_many :haves, class_name: 'Have'
+	has_many :have_items, through: :haves, class_name: 'Item', source: :item
+	
 
   def want(item)
     self.wants.find_or_create_by(item_id: item.id)
@@ -22,3 +24,16 @@ end
   def want?(item)
     self.want_items.include?(item)
   end
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id)
+    have.destroy if have
+  end
+
+  def have?(item)
+    self.have_items.include?(item)
+  end
+end
